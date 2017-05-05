211d665820429038895a0685eedeab78f676ab6d
Sebastien Phaneuf <sebastien.phaneuf@autodesk.com>
2017-05-05 17:41:41 -0400

    sclae experiment

--- canvas-renderer.ts
+++ canvas-renderer.ts
@@ -6,6 +6,12 @@ export class CanvasRenderer {
     scale: number;
     x: number = 0;
     y: number = 0;
+    left: number = 0;
+    right: number = 0;
+    top: number = 0;
+    bottom: number = 0;
+    width: number = 0;
+    height: number = 0;
     constructor(canvas: HTMLCanvasElement) {
         this.canvas = canvas;
         this.context = canvas.getContext('2d');
@@ -19,7 +25,7 @@ export class CanvasRenderer {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.context.translate(this.canvas.width / 2, this.canvas.height / 2);
         this.context.scale(this.scale, -this.scale);
-        this.context.translate(this.x, this.y);
+        // this.context.translate(this.x, this.y);
 
         for (let s of shapes) {
             this.drawShape(s);
@@ -31,8 +37,7 @@ export class CanvasRenderer {
 
         this.context.transform.apply(this.context, shape.transform);
         this.context.fillStyle = 'rgba(' + utils.hsv2rgb(shape.color) + ')';
-        this.context.scale(1.025, 1.025);
-        
+
         switch(shape.shape) {
             case 'SQUARE':
                 this.drawSquare();
--- docs/TODO.md
+++ docs/TODO.md
@@ -1,12 +1,30 @@
 # TODO
 
-- Separate grammar parser (use jison), json builder, evaluator and renderer
+## Goals
 - keep first pass really simple:
     - loop
     - rule with recursion
     - no target, no transform
     - what to do with functions (sin, cos, ...) -> nothing for now.
 - app with some examples (selector)
+
+### Tasks
+- stop recursion on shape too small
+- better handling of scaling + potential scaling according to shape area?
+- different evaluator strat (with requestAnimationFrame)
+- different rendering strat (with requestAnimationFrame)
+- background
+- ensure that grammar can have animation (with a cancel button)
+- examples selector
+- rule parser
+
+### Stats
+- nb of times each rule is evaluated
+- nb of shapes drawn
+- stats on the different algo:
+    - shapes processing time (if finite)
+    - drawing processing time (if finite)
+    - frame counter (fps?)
 
 ### Modes of evaluation
 - Compute all shapes, render all shapes
